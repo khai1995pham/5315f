200ffb77c7cfdfe5b1d3a183b4a9169fc7e36c37
phamd4 <phamd4@seattleu.edu>
2018-10-17 02:11:19 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -5,91 +5,91 @@
       "Last Name": "Buton",
       "Receivable": "$61240",
       "Average Days Outstanding": "175",
-      "Phone Number": "428-307-6386",
+      "Phone Number": "428-307-6386"
     },
     {
       "First Name": "Kristopher",
       "Last Name": "Giguere",
       "Receivable": "$24414",
       "Average Days Outstanding": "116",
-      "Phone Number": "475-114-4845",
+      "Phone Number": "475-114-4845"
     },
     {
       "First Name": "Sharlene",
       "Last Name": "Wolframe",
       "Receivable": "$12456",
       "Average Days Outstanding": "54",
-      "Phone Number": "425-548-5613",
+      "Phone Number": "425-548-5613"
     },
     {
       "First Name": "Stephen",
       "Last Name": "Tyler",
       "Receivable": "$125478",
       "Average Days Outstanding": "5",
-      "Phone Number": "513-548-9769",
+      "Phone Number": "513-548-9769"
     },
     {
       "First Name": "Josh",
       "Last Name": "Groban",
       "Receivable": "$121566161",
       "Average Days Outstanding": "100",
-      "Phone Number": "425-158-6485",
+      "Phone Number": "425-158-6485"
     },
     {
       "First Name": "Don",
       "Last Name": "Omas",
       "Receivable": "$51",
       "Average Days Outstanding": "2",
-      "Phone Number": "986-168-1555",
+      "Phone Number": "986-168-1555"
     },
     {
       "First Name": "Ron",
       "Last Name": "Gaard",
       "Receivable": "$1658",
       "Average Days Outstanding": "23",
-      "Phone Number": "168-565-9122",
+      "Phone Number": "168-565-9122"
     },
     {
       "First Name": "Molly",
       "Last Name": "Sun",
       "Receivable": "$516",
       "Average Days Outstanding": "23",
-      "Phone Number": "981-574-9795",
+      "Phone Number": "981-574-9795"
     },
     {
       "First Name": "Danielle",
       "Last Name": "Summers",
       "Receivable": "$16889",
       "Average Days Outstanding": "10",
-      "Phone Number": "797-416-6548",
+      "Phone Number": "797-416-6548"
     },
     {
       "First Name": "Emily",
       "Last Name": "Roberts",
       "Receivable": "$100158",
       "Average Days Outstanding": "81",
-      "Phone Number": "563-546-8796",
+      "Phone Number": "563-546-8796"
     },
     {
       "First Name": "Julia",
       "Last Name": "Manchild",
       "Receivable": "$29",
       "Average Days Outstanding": "4",
-      "Phone Number": "428-598-5887",
+      "Phone Number": "428-598-5887"
     },
     {
       "First Name": "Dan",
       "Last Name": "Dapper",
       "Receivable": "$98155",
       "Average Days Outstanding": "26",
-      "Phone Number": "442-555-9732",
+      "Phone Number": "442-555-9732"
     },
     {
       "First Name": "Jonathan",
       "Last Name": "Rand",
       "Receivable": "$16584652",
       "Average Days Outstanding": "179",
-      "Phone Number": "485-846-6185",
+      "Phone Number": "485-846-6185"
     }
   ]
 }
